a36cb8fe6ddb9a5360a531798cc85a01cbc62e13
BorovChe <roman2000borov@gmail.com>
2026-02-10 16:09:36 +0200

    EED-7 [FE] Modal+button "Contact us"

--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
 
 const Logo = () => {
-  return <Image src="/UI/logo.svg" alt="Logo" width={46} height={40} />;
+  return (
+    <div className="relative w-11.5 h-10">
+      <Image src="/UI/logo.png" alt="Logo" fill />
+    </div>
+  );
 };
 
 export default Logo;
