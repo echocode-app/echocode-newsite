8634cc6c569afd92d40af4c9e293ff356aa0f02f
BorovChe <roman2000borov@gmail.com>
2026-02-19 12:47:35 +0200

    EED-24 [FE] Growth & Marketing Section

--- src/app/(public)/service-direction/mobile-development/page.tsx
+++ src/app/(public)/service-direction/mobile-development/page.tsx
@@ -1,6 +1,7 @@
 import FullCycleSection from '@/components/sections/directions/mobile/FullCycleSection';
 import HeroSection from '@/components/sections/directions/mobile/HeroSection';
-// import SpecializationSection from '@/components/sections/directions/mobile/SpecializationSection';
+import MarketingSection from '@/components/sections/directions/mobile/MarketingSection';
+import SpecializationSection from '@/components/sections/directions/mobile/SpecializationSection';
 import StaticGradientLine from '@/components/UI/StaticGradientLine';
 
 const Mobile = () => {
@@ -9,7 +10,8 @@ const Mobile = () => {
       <HeroSection />
       <StaticGradientLine />
       <FullCycleSection />
-      {/* <SpecializationSection /> */}
+      <SpecializationSection />
+      <MarketingSection />
     </>
   );
 };
---
+++ src/components/sections/directions/mobile/MarketingSection/MarketingSection.tsx
@@ -0,0 +1,26 @@
+import SectionContainer from '@/components/UI/section/SectionContainer';
+import SectionGradientLine from '@/components/UI/section/SectionGradientLine';
+import SectionTitle from '@/components/UI/section/SectionTitle';
+import MarketingList from './MarketingList';
+
+const MarketingSection = () => {
+  return (
+    <section className="pb-10 md:pb-2">
+      <SectionGradientLine height="1" />
+      <SectionContainer>
+        <div className="mb-2.5">
+          <SectionTitle>
+            GROWTH <span className="text-[12px] md:text-[18px]">&</span> MARKETING
+          </SectionTitle>
+        </div>
+        <p className="mb-10 text-main-sm">
+          Professional development is only half of the story. We help products become visible in
+          stores using proven optimization methods and strategic promotion.
+        </p>
+        <MarketingList />
+      </SectionContainer>
+    </section>
+  );
+};
+
+export default MarketingSection;
