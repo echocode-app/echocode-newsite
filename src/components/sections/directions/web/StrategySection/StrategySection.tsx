cf3c3405e206d9288ffaaea9420f5cb2128856b2
BorovChe <roman2000borov@gmail.com>
2026-02-19 16:01:57 +0200

    EED-26 [FE] Expertise & Strategy Section

---
+++ src/components/sections/directions/web/StrategySection/StrategySection.tsx
@@ -0,0 +1,28 @@
+import SectionContainer from '@/components/UI/section/SectionContainer';
+import SectionTitle from '@/components/UI/section/SectionTitle';
+import MetricsBlock from './MetricsBlock';
+
+const StrategySection = () => {
+  return (
+    <section className="pt-10 pb-10 md:pb-27.5">
+      <SectionContainer>
+        <div className="flex items-start justify-between flex-col lg:flex-row">
+          <div className="max-w-100 md:max-w-150 lg:max-w-120 mb-10 lg:mb-0">
+            <div className="mb-2.5">
+              <SectionTitle>
+                EXPERTISE <span className="text-[12px] md:text-[18px]">&</span> STRATEGY
+              </SectionTitle>
+            </div>
+            <p className=" text-main-xs md:text-main-sm">
+              Every line of code is a business asset. We focus on architectural integrity to
+              minimize technical debt and maximize ROI.
+            </p>
+          </div>
+          <MetricsBlock />
+        </div>
+      </SectionContainer>
+    </section>
+  );
+};
+
+export default StrategySection;
